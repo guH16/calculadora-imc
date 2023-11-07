2c4f41d213b7eaee6d0f399fe45f21d2f890a20d
guH16 <gustavosampaiosamadello@hotmail.com>
2023-11-07 14:29:21 -0300

    Calculadora de IMC v1.1 (CSS Básico)

--- src/componentes/Calcular/index.js
+++ src/componentes/Calcular/index.js
@@ -7,7 +7,7 @@ function Calcula() {
 
   const calculaIMC = () => {
     const peso1 = parseFloat(peso);
-    const altura1 = parseFloat(altura) / 100; // Converter altura de cm para metros
+    const altura1 = parseFloat(altura) / 100; 
 
     if (peso1 && altura1) {
       const resultado = peso1 / (altura1 * altura1);
@@ -16,17 +16,13 @@ function Calcula() {
         setImc(`Seu IMC é ${resultado.toFixed(2)} = Magro`);
       } else if (resultado <= 24.9 && resultado > 18.5) {
         setImc(`Seu IMC é ${resultado.toFixed(2)} = Padrão`);
-      }
-      else if (resultado <= 25 && resultado >= 29.9) {
+      } else if (resultado <= 29.9) {
         setImc(`Seu IMC é ${resultado.toFixed(2)} = Sobrepeso`);
-      }
-      else if (resultado <= 30 && resultado >= 34.9) {
+      } else if (resultado <= 34.9) {
         setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade I`);
-      }
-      else if (resultado <= 35 && resultado >= 39.9) {
+      } else if (resultado <= 39.9) {
         setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade II`);
-      }
-      else if (resultado >= 40) {
+      } else {
         setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade III`);
       }
     } else {
@@ -35,19 +31,23 @@ function Calcula() {
   };
 
   return (
-    <div>
+    <div className='flex flex-col  justify-center'>
       <input
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
         type="number"
         placeholder="Digite seu Peso / Ex: 90"
         onChange={(e) => setPeso(e.target.value)}
       />
       <input
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
         type="number"
         placeholder="Digite sua Altura (cm) / Ex: 189"
         onChange={(e) => setAltura(e.target.value)}
       />
-      <button onClick={calculaIMC}>Calcular</button>
-      {imc && <p>{imc}</p>}
+      <button onClick={calculaIMC}
+      class="botao-com-cor border rounded-md font-mono"
+      >Calcular</button>
+      {imc && <p className='corBrancoTexto'>{imc}</p>}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./public/index.html"],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html", './components/**/*.{html,js}',],
   theme: {
+    colors:{
+      'gss-azul': '#374151'
+    },
     extend: {},
   },
   plugins: [],
