807446442cde74c687c594be30789dd74c878697
guH16 <gustavosampaiosamadello@hotmail.com>
2023-11-07 16:02:29 -0300

    Calculadora de IMC v1.2(Imagens)

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Calcula from './componentes/Calcular'
 function App() {
   return (
     <body className="bg-gss-azul">
-    <div class="flex place-content-center items-center h-[100vh] flex flex-col" >
-      <p className="corBrancoTexto text-5xl mb-5 font-mono">Calcule seu IMC</p>
+    <div className="flex place-content-center items-center h-[100vh] flex flex-col" >
+      <p className="corBrancoTexto text-5xl mb-5 font-mono p-5">Calcule seu IMC</p>
      <Calcula></Calcula>
     </div>
     </body>
--- src/componentes/Calcular/index.js
+++ src/componentes/Calcular/index.js
@@ -1,29 +1,44 @@
 import React, { useState } from 'react';
+import Magro from '../../imagens/Magro.webp';
+import Padrao from '../../imagens/Padrao.webp';
+import Sobrepeso from '../../imagens/Sobrepeso.webp';
+import ObesidadeI from '../../imagens/Obesidade1.webp';
+import ObesidadeII from '../../imagens/Obesidade2.webp';
+import ObesidadeIII from '../../imagens/Obesidade3.webp';
 
 function Calcula() {
   const [peso, setPeso] = useState('');
   const [altura, setAltura] = useState('');
   const [imc, setImc] = useState('');
+  const [imagemIMC, setImcImage] = useState(null);
 
   const calculaIMC = () => {
     const peso1 = parseFloat(peso);
     const altura1 = parseFloat(altura) / 100; 
-
+    
+    
     if (peso1 && altura1) {
+
       const resultado = peso1 / (altura1 * altura1);
 
       if (resultado <= 18.5) {
-        setImc(`Seu IMC é ${resultado.toFixed(2)} = Magro`);
+        setImc(`Seu IMC é ${resultado.toFixed(2)} = Magro - Amarra que o vento ta forte`);
+        setImcImage(Magro)
       } else if (resultado <= 24.9 && resultado > 18.5) {
-        setImc(`Seu IMC é ${resultado.toFixed(2)} = Padrão`);
+        setImc(`Seu IMC é ${resultado.toFixed(2)} = Padrão - Boa Chefe`);
+        setImcImage(Padrao)
       } else if (resultado <= 29.9) {
-        setImc(`Seu IMC é ${resultado.toFixed(2)} = Sobrepeso`);
+        setImc(`Seu IMC é ${resultado.toFixed(2)} = Sobrepeso - Partiu Esteira`);
+        setImcImage(Sobrepeso)
       } else if (resultado <= 34.9) {
-        setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade I`);
+        setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade I - Cancela o Rodizio`);
+        setImcImage(ObesidadeI)
       } else if (resultado <= 39.9) {
-        setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade II`);
+        setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade II - Ja não vê o piru, ne?`);
+        setImcImage(ObesidadeII)
       } else {
-        setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade III`);
+        setImc(`Seu IMC é ${resultado.toFixed(2)} = Obesidade III - Encomenda o Caixão Thais Carla`);
+        setImcImage(ObesidadeIII)
       }
     } else {
       setImc('Por favor, insira valores válidos.');
@@ -33,21 +48,22 @@ function Calcula() {
   return (
     <div className='flex flex-col  justify-center'>
       <input
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white mb-5"
         type="number"
         placeholder="Digite seu Peso / Ex: 90"
         onChange={(e) => setPeso(e.target.value)}
       />
       <input
-      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white mb-5"
         type="number"
         placeholder="Digite sua Altura (cm) / Ex: 189"
         onChange={(e) => setAltura(e.target.value)}
       />
       <button onClick={calculaIMC}
-      class="botao-com-cor border rounded-md font-mono"
+      className="botao-com-cor border rounded-md font-mono mb-5"
       >Calcular</button>
-      {imc && <p className='corBrancoTexto'>{imc}</p>}
+      {imc && <p className='corBrancoTexto mb-5'>{imc}</p>}
+      {imagemIMC && <img src={imagemIMC} alt="Imagem do IMC" />}
     </div>
   );
 }
